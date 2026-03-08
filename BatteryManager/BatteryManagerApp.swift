93ccacd44a24fa35fcef6cac8fde14fccec39d64
Dhanilka <dhanilkaid@gmail.com>
2026-03-08 21:21:12 +0530

    Initial Commit

---
+++ BatteryManager/BatteryManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryManagerApp.swift
+//  BatteryManager
+//
+//  Created by Dhanilka Dasanayake on 3/8/26.
+//
+
+import SwiftUI
+
+@main
+struct BatteryManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
